0c628ef069c0367753642041b151d4f862354993
Mateus Prado <mateusp2000@hotmail.com>
2024-11-09 15:21:20 -0300

    feat: new application

---
+++ README.md
@@ -0,0 +1,12 @@
+NodeJS application developed using SOLID patterns and TDD.
+Tutorial by RocketSeat.
+
+To install the dependencies, use:
+```
+npm install
+```
+
+To run the tests, use:
+```
+npm test or npx jest
+```
---
+++ src/application/usecases/create-challenge-submission.spec.ts
@@ -0,0 +1,38 @@
+import { InMemoryChallengesRepository } from "../../../tests/repositories/in-memory-challenges-repository";
+import { InMemoryStudentsRepository } from "../../../tests/repositories/in-memory-students-repository";
+import { Challenge } from "../../domain/entities/challenge";
+import { Student } from "../../domain/entities/student";
+import { CreateChallengeSubmission } from "./create-challenge-submission"
+
+describe('Create challenge submission use case', () => {
+    it('should be able to create a new challenge submission', async () => {
+        const studentsRepository = new InMemoryStudentsRepository();
+        const challengesRepository = new InMemoryChallengesRepository();
+
+        const student = Student.create({
+            name: 'Diego',
+            email: 'doe@example.com',
+        })
+
+        const challenge = Challenge.create({
+            instructionsUrl: 'http://example.com',
+            title: 'Challenge 01'
+        })
+
+        studentsRepository.items.push(student);
+        challengesRepository.items.push(challenge);
+        
+        const sut = new CreateChallengeSubmission(
+            studentsRepository,
+            challengesRepository
+        );
+
+        const response = await sut.execute({
+            studentId: student.id,
+            challengeId: challenge.id,
+        })
+
+
+        expect(response).toBeTruthy()
+    });
+});
